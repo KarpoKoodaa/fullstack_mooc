be8a8a7b21ecfa4cbc5196bf352557a8826af238
KarpoKoodaa <karpokoodaa@gmail.com>
2025-07-10 16:57:15 +0300

    osa1 tehtävä 1.3

--- osa1/kurssitiedot/src/App.jsx
+++ osa1/kurssitiedot/src/App.jsx
@@ -28,20 +28,29 @@ const Total = (props) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamental of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 =  {
+    name: 'Fundamental of React',
+    exercises: 10
+  }
+
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  } 
+
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+
+  } 
 
   return (
     <div>
       <Header course={course}/>
-      <Part part={part1} points={exercises1} /> 
-      <Part part={part2} points={exercises2} /> 
-      <Part part={part3} points={exercises3} /> 
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Part part={part1.name} points={part1.exercises} /> 
+      <Part part={part2.name} points={part2.exercises} /> 
+      <Part part={part3.name} points={part3.exercises} /> 
+      <Total exercises1={part1.exercises} exercises2={part2.exercises} exercises3={part3.exercises} />
     </div>
   )
 }
